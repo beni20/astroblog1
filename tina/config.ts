b77d1d209a59ae4c948dc583d5f7473d74505992
y architect <64083403+beni20@users.noreply.github.com>
2024-07-10 14:17:06 +0200

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -5,13 +5,12 @@ import { defineConfig } from 'tinacms'
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
-	branch:
-		    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-    		    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-    	            process.env.HEAD!, // Netlify branch env,
-	clientId: '41b866a9-5f00-4f0d-af8e-edd3e139eeb0', // Get this from tina.io
-	token: '80660f6863650ca802de79770c5844247cdd35a9', // Get this from tina.io
-
+	  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
+	  branch:
+	    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
+	    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
+	    process.env.HEAD!, // Netlify branch env
+	  token: process.env.TINA_TOKEN!,
 	build: {
 		outputFolder: 'admin',
 		publicFolder: 'public'
