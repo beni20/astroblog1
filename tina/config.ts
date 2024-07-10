2842e53225b056537446c5a8237a9faf134cb016
y architect <64083403+beni20@users.noreply.github.com>
2024-07-10 15:04:41 +0200

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -6,10 +6,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
 	  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-	  branch:
-	    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-	    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-	    process.env.HEAD!, // Netlify branch env
+	  branch: 'main',
 	  token: process.env.TINA_TOKEN!,
 	build: {
 		outputFolder: 'admin',
